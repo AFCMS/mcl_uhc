44766376583a7bb250e964a6fb9a9383e1435d45
AFCMS <afcm.contact@gmail.com>
2021-04-12 16:55:15 +0200

    add auto enchants on crafting

---
+++ enchantments.lua
@@ -0,0 +1,25 @@
+mcl_uhc.enchantments = {
+    ["pickaxe"] = {
+        {name="unbreaking", level=1}
+        {name="efficiency", level=3}
+    }
+    ["shovel"] = {
+        {name="unbreaking", level=1}
+        {name="efficiency", level=3}
+    }
+    ["axe"] = {
+        {name="unbreaking", level=1}
+        {name="efficiency", level=3}
+    }
+}
+
+minetest.register_on_craft(function(itemstack, player, old_craft_grid, craft_inv)
+    for group, enchants in pairs(mcl_uhc.enchantments) do
+        if minetest.get_item_group(itemstack:get_name(), group) then
+            for _,enchant in pairs(enchants) do
+                itemstack = mcl_enchanting.enchant(itemstack, enchant.name, enchant.level)
+            end
+        end
+    end
+    return itemstack
+end)
--- init.lua
+++ init.lua
@@ -1,3 +1,6 @@
 local modpath = minetest.get_modpath(minetest.get_current_modname())
 
+mcl_uhc = {}
+
 dofile(modpath.."/drops.lua")
+dofile(modpath.."/enchantments.lua")
