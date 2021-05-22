1b3412a7140cff5679991f9648395a7e3014cb59
AFCMS <afcm.contact@gmail.com>
2021-05-22 17:56:49 +0200

    make coal drop 4 torches

--- drops.lua
+++ drops.lua
@@ -1,2 +1,3 @@
+minetest.override_item("mcl_core:stone_with_coal", {drop = 'mcl_torches:torch 4'})
 minetest.override_item("mcl_core:stone_with_iron", {drop = 'mcl_core:iron_ingot'})
 minetest.override_item("mcl_core:stone_with_gold", {drop = 'mcl_core:gold_ingot'})
