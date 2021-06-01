8e66aeffffa50e59f237c36e93f78f61100b7af4
AFCMS <afcm.contact@gmail.com>
2021-06-01 14:07:07 +0200

    fix crash then trying to craft non tool object

--- enchantments.lua
+++ enchantments.lua
@@ -15,7 +15,7 @@ mcl_uhc.enchantments = {
 
 minetest.register_on_craft(function(itemstack, player, old_craft_grid, craft_inv)
     for group, enchants in pairs(mcl_uhc.enchantments) do
-        if minetest.get_item_group(itemstack:get_name(), group) then
+        if minetest.get_item_group(itemstack:get_name(), group) > 0 then
             for _,enchant in pairs(enchants) do
                 itemstack = mcl_enchanting.enchant(itemstack, enchant.name, enchant.level)
             end
